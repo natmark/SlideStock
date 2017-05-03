6fe4c6c0d0a4e44873b51fcc2b3e750c44a6d3b6
natmark <natmark0918@gmail.com>
2017-05-03 17:05:56 +0900

    :open_file_folder: organize project files

---
+++ SlideStock/ViewModels/StockViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StockViewModel.swift
+//  SlideStock
+//
+//  Created by AtsuyaSato on 2017/05/03.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class StockViewModel: NSObject {
+
+}
